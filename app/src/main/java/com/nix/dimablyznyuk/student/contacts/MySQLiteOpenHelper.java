9d252224ef2d96212a2dc4515863478ef58d857e
Dima <mittay2006@gmail.com>
2015-07-13 10:55:14 +0300

    Initial

--- app/src/main/java/com/nix/dimablyznyuk/student/contacts/MySQLiteOpenHelper.java
+++ app/src/main/java/com/nix/dimablyznyuk/student/contacts/MySQLiteOpenHelper.java
@@ -3,5 +3,44 @@ package com.nix.dimablyznyuk.student.contacts;
 /**
  * Created by Dima Blyznyuk on 09.07.15.
  */
-public class MySQLiteOpenHelper {
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_ADDRESS = "address";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_GENDER = "gender";
+
+    private static final String DATABASE_NAME = "contacts.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_CONTACTS + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NAME
+            + " text not null, " + COLUMN_ADDRESS
+            + " text not null, " + COLUMN_PHONE
+            + " text not null, " + COLUMN_GENDER
+            + " text not null);";
+
+    public MySQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(db);
+    }
 }
